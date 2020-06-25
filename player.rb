33a75a5f52132aa184cc84f98544c13f77d5bca3
Anakin Skywalker <mutovin9@mail.ru>
2020-06-25 00:47:31 -0400

    'Урок 11.Проект "Игра Black Jack".Добавлен класс игрока'

---
+++ player.rb
@@ -0,0 +1,47 @@
+class Player
+  attr_accessor :money, :cards, :score
+  attr_reader :name
+
+  CARDS_TYPES = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  CARDS_SUITS = %w[+ <3 ^ <>].freeze
+
+  def initialize(name)
+    @name = name
+    @money = 100
+    @cards = []
+    @score = 0
+    give_start_cards
+  end
+
+  def take_bet
+    self.money -= 10
+  end
+
+  private
+
+  def give_start_cards
+    2.times { add_card }
+  end
+
+  def add_card
+    card_type = CARDS_TYPES[rand(CARDS_TYPES.length)]
+    card_suit = CARDS_SUITS[rand(CARDS_SUITS.length)]
+    card = "#{card_type}#{card_suit}"
+    cards << card
+    calculate_score(card_type)
+  end
+
+  def calculate_score(card_type)
+    if card_type == 'A'
+      (score + 11) > 21 ? add_score(1) : add_score(11)
+    elsif %w[J K Q].include?(card_type)
+      add_score(10)
+    else
+      add_score(card_type.to_i)
+    end
+  end
+
+  def add_score(value)
+    self.score += value
+  end
+end
